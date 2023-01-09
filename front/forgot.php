beacf1123382df5fb949a00b9c80bfe60d55e381
ruzs <eddie10913@gmail.com>
2023-01-09 14:51:46 +0800

    完成忘記密碼功能

--- front/forgot.php
+++ front/forgot.php
@@ -1,1 +1,14 @@
-忘記密碼
+<div class="box">
+  <div>請輸入信箱以查詢密碼</div>
+  <div><input type="text"name="email"id="email"></div>
+  <div id="result"></div>
+  <div><button onclick="forgot()">尋找</button></div>
+</div>
+<script>
+  function forgot() {
+    $("#email").val();
+    $.get('./api/forgot.php',{email:$("#email").val()},(result)=>{
+      $("#result").html(result);
+    })
+  }
+</script>
