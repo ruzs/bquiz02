08d4f00ae60a908932dbfffcfd11d3e6b5c1f400
ruzs <eddie10913@gmail.com>
2023-01-09 11:35:25 +0800

    在登入頁撰寫登入頁js程式碼

--- front/login.php
+++ front/login.php
@@ -1,1 +1,58 @@
-登入
+<fieldset>
+  <legend>會員登入</legend>
+  <table>
+    <tr>
+      <td>帳號</td>
+      <td><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+      <td>密碼</td>
+      <td><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+      <td>
+        <button onclick="login()">登入</button>
+        <button onclick="reset()">清除</button>
+      </td>
+      <td>
+        <a href="?do=forgot">忘記密碼</a>
+        <a href="?do=reg">尚未註冊</a>
+      </td>
+    </tr>
+  </table>
+</fieldset>
+<script>
+  function reset() {
+    $("#acc,#pw").val("");
+    console.log("reset OK");
+  }
+
+  function login() {
+    let user = {
+      acc: $("#acc").val(),
+      pw: $("#pw").val()
+    }
+    $.post(".api/chk_acc.php", user, (result) => {
+      if (parseInt(result === 1)) {
+        //有此帳號
+        $.post(".api/chk_acc.php", user, (result) => {
+          if (parseInt(result === 1)) {
+            // 帳密正確
+            if (user.acc === "admin") {
+              location.href = 'back.php';
+            } else {
+              location.href = 'index.php';
+            }
+          } else {
+            // 密碼錯誤
+            alert("密碼錯誤");
+          }
+        })
+      } else {
+        //無此帳號
+        alert("查無此帳號");
+      }
+    })
+    console.log(user);
+  }
+</script>
