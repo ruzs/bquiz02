267d7911ba203687631938c0c590cd10898678d2
ruzs <eddie10913@gmail.com>
2023-01-13 10:47:21 +0800

    完成後台文章管理功能

--- back/news.php
+++ back/news.php
@@ -1,6 +1,13 @@
 <?php
-$rows=$News->all();
+$all=$News->count();
+$div=3;
+$pages=ceil($all/$div);
+$now=$_GET['p']??1;
+$start=($now-1)*$div;
+
+$rows=$News->all(" limit $start , $div");
 ?>
+<form action="./api/edit.php" method="post">
 <table>
   <tr>
     <td>編號</td>
@@ -12,9 +19,9 @@ $rows=$News->all();
   foreach ($rows as $key => $row) {
   ?>
   <tr>
-    <td></td>
-    <td></td>
-    <td><input type="checkbox" name="sh[]" value="<?=$row['id']?>"></td>
+    <td><?=$key+1;?>.</td>
+    <td><?=$row['title'];?></td>
+    <td><input type="checkbox" name="sh[]" value="<?=$row['id']?>" <?=($row['sh']==1)?'checked':'';?>></td>
     <td><input type="checkbox" name="del[]" value="<?=$row['id']?>"></td>
     <input type="hidden" name="id[]"value="<?=$row['id']?>">
   </tr>
@@ -22,3 +29,25 @@ $rows=$News->all();
   }
   ?>
 </table>
+<div class="ct">
+<?php
+if(($now-1)>0){
+    $pre=$now-1;
+    echo "<a href='back.php?do=news&p=$pre'> < </a>";
+}
+
+for($i=1; $i<=$pages;$i++){
+    $size=($i==$now)?"24px":"16px";
+    echo "<a href='back.php?do=news&p=$i' style='font-size:$size'> $i </a>";
+}
+
+if(($now+1)<=$pages){
+    $next=$now+1;
+    echo "<a href='back.php?do=news&p=$next'> > </a>";
+}
+?>
+
+
+</div>
+<div class="ct"><input type="submit" value="確定修改"></div>
+</form>
