4c90dc2ff9a79729be48b6725e252b9a8ed147ce
ruzs <eddie10913@gmail.com>
2023-01-16 09:28:42 +0800

    為pop完成人氣文章區的資料顯示 & 完成最新文章區的點擊展示完整內容功能

--- front/pop.php
+++ front/pop.php
@@ -1,12 +1,3 @@
-<style>
-  .full{
-    display:none;
-  }
-  .news-title{
-    cursor: pointer;
-    background-color: #eee;
-  }
-</style>
 <fieldset>
   <legend>目前位置:首頁 > 最新文章區</legend>
   <table>
@@ -25,11 +16,8 @@
     foreach($rows as $row){
       ?>
     <tr>
-      <td class="news-title"><?=$row['title'];?></td>
-      <td>
-        <div  class="short"><?=mb_substr($row['text'],0,20);?>...</div>
-        <div  class="full"><?=($row['text']);?></div>
-      </td>
+      <td><?=$row['title'];?></td>
+      <td><?=mb_substr($row['text'],0,20);?>...</td>
       <td></td>
     </tr>
       <?php
@@ -40,25 +28,18 @@
     <?php
     if(($now-1)>0){
         $prev=$now-1;
-        echo "<a href='index.php?do=news&p=$prev'> < </a>";
+        echo "<a href='index.php?do=pop&p=$prev'> < </a>";
     }
     
     for($i=1; $i<=$pages;$i++){
         $size=($i==$now)?"26px":"16px";
-        echo "<a href='index.php?do=news&p=$i' style='font-size:$size'> $i </a>";
+        echo "<a href='index.php?do=pop&p=$i' style='font-size:$size'> $i </a>";
     }
     
     if(($now+1)<=$pages){
         $next=$now+1;
-        echo "<a href='index.php?do=news&p=$next'> > </a>";
+        echo "<a href='index.php?do=pop&p=$next'> > </a>";
     }
     ?>
   </div>
 </fieldset>
-<script>
-  $(".news-title").on("click",function(){
-    $(this).next().children('.short').toggle();
-    $(this).next().children('.full').toggle();
-
-  })
-</script>
